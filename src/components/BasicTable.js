7db39676a9035d98dc0a0b8328c696ac17badbb7
MosNes <7hekarl@gmail.com>
2023-10-30 15:13:15 -0400

    scaffolded basic table component

---
+++ src/components/BasicTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './columns';
+
+export const BasicTable = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
